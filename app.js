fc3cc6785c19b0aced2081aa3326a2f8eda33c5e
KravKo <86314507+KravKo@users.noreply.github.com>
2023-12-18 17:14:44 +0545

    innn

--- app.js
+++ app.js
@@ -28,35 +28,35 @@ window.addEventListener("scroll", () => {
     });
 });
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const textArray = ["Web Developer", "Student", "Programmer", "Future Game Developer"];
-        let currentTextIndex = 0;
-        let currentText = '';
-        let letterIndex = 0;
-        let texttyping = document.getElementById('typing-text');
+document.addEventListener('DOMContentLoaded', function () {
+    const textArray = ["Web Developer", "Student", "Programmer", "Future Game Developer"];
+    let currentTextIndex = 0;
+    let currentText = '';
+    let letterIndex = 0;
+    let texttyping = document.getElementById('typing-text');
 
-        function type() {
-            if (letterIndex < textArray[currentTextIndex].length) {
-                currentText += textArray[currentTextIndex][letterIndex];
-                texttyping.innerText = currentText;
-                letterIndex++;
-                setTimeout(type, 150);
-            } else {
-                setTimeout(erase, 1000);
-            }
+    function type() {
+        if (letterIndex < textArray[currentTextIndex].length) {
+            currentText += textArray[currentTextIndex][letterIndex];
+            texttyping.innerText = currentText;
+            letterIndex++;
+            setTimeout(type, 150);
+        } else {
+            setTimeout(erase, 1000);
         }
+    }
 
-        function erase() {
-            if (letterIndex > 0) {
-                currentText = textArray[currentTextIndex].substring(0, letterIndex - 1);
-                texttyping.innerText = currentText;
-                letterIndex--;
-                setTimeout(erase, 75);
-            } else {
-                currentTextIndex = (currentTextIndex + 1) % textArray.length;
-                setTimeout(type, 500);
-            }
+    function erase() {
+        if (letterIndex > 0) {
+            currentText = textArray[currentTextIndex].substring(0, letterIndex - 1);
+            texttyping.innerText = currentText;
+            letterIndex--;
+            setTimeout(erase, 75);
+        } else {
+            currentTextIndex = (currentTextIndex + 1) % textArray.length;
+            setTimeout(type, 500);
         }
+    }
 
-        type();
-    });
+    type();
+});
